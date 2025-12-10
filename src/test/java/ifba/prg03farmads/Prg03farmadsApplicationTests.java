4424ce42fa80656629d0741a85a3042df3ba6968
The-Kryz <dudanovais34@gmail.com>
2025-12-10 14:12:25 -0300

    fix: Corrigida configuração de testes Spring Boot e verificado BD.

--- src/test/java/ifba/prg03farmads/Prg03farmadsApplicationTests.java
+++ src/test/java/ifba/prg03farmads/Prg03farmadsApplicationTests.java
@@ -2,8 +2,9 @@ package ifba.prg03farmads;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import br.com.ifba.Prg03farmadsApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = Prg03farmadsApplication.class) 
 class Prg03farmadsApplicationTests {
 
 	@Test

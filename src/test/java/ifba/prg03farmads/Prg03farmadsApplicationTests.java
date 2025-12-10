a2d0d219b4da02053f536c4974b29ec967039e8a
ianep <ianep@LAPTOP-LBK04C1B>
2025-12-10 09:40:40 -0300

    commit inicial do projeto

---
+++ src/test/java/ifba/prg03farmads/Prg03farmadsApplicationTests.java
@@ -0,0 +1,13 @@
+package ifba.prg03farmads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg03farmadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

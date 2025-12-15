d299b469854aeb34ba7cf8822cc8f7f608d2d51d
20222ireads0016-git <20222ireads0016@ifba.edu.br>
2025-12-15 20:55:03 -0300

    Criando as Interfaces, Classes e telas (vazios) necessarios para a implementação das receitas

---
+++ src/main/java/br/com/ifba/receita/entity/Receita.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.receita.entity;
+
+import br.com.ifba.infraestructure.entity.PersistenceEntity;
+
+/**
+ *
+ * @author paulo
+ */
+public class Receita extends PersistenceEntity{
+    
+}

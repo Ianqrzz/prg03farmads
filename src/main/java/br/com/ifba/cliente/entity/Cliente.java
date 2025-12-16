9c4d941b2fb0d14b11d2856a1e0642a10250d557
ianep <ianep@LAPTOP-LBK04C1B>
2025-12-16 20:29:46 -0300

    Alterando Classe cliente e Receita para o programa rodar

--- src/main/java/br/com/ifba/cliente/entity/Cliente.java
+++ src/main/java/br/com/ifba/cliente/entity/Cliente.java
@@ -4,10 +4,21 @@
  */
 package br.com.ifba.cliente.entity;
 
+import br.com.ifba.infraestructure.entity.PersistenceEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  * @author paulo, Criei a classe em branco somente para implementar em receitas
  */
-public class Cliente {
+@Entity
+@Getter
+@Setter
+public class Cliente extends PersistenceEntity{
     
-}
+    public Cliente(){
+    }
+  }
+

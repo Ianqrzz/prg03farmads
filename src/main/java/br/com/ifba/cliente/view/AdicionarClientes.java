e0c67bbeb8d1e99dcf016b90837c0531a8090f8d
PC <PC@DESKTOP-3NNTM0F>
2026-02-02 15:52:18 -0300

    corrigido o erro de adicionar clientes não salvar o email

--- src/main/java/br/com/ifba/cliente/view/AdicionarClientes.java
+++ src/main/java/br/com/ifba/cliente/view/AdicionarClientes.java
@@ -62,20 +62,20 @@ public class AdicionarClientes extends javax.swing.JFrame {
             }
         });
 
-        txtCpf.setText("jTextField1");
+        txtCpf.setText("CPF");
 
-        txtEmail.setText("jTextField2");
+        txtEmail.setText("Email");
 
-        txtTelefone.setText("jTextField3");
+        txtTelefone.setText("Telefone");
 
-        txtNome.setText("jTextField4");
+        txtNome.setText("Nome");
         txtNome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtNomeActionPerformed(evt);
             }
         });
 
-        txtEndereco.setText("jTextField5");
+        txtEndereco.setText("Endereço");
         txtEndereco.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtEnderecoActionPerformed(evt);
@@ -157,7 +157,7 @@ public class AdicionarClientes extends javax.swing.JFrame {
         
         novo.setCpf(txtCpf.getText());
         
-        novo.setEmail(txtEmail.getName());
+        novo.setEmail(txtEmail.getText());
         
         novo.setTelefone(txtTelefone.getText());
         
@@ -195,27 +195,7 @@ public class AdicionarClientes extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new AdicionarClientes().setVisible(true));
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSalvar;
